d938581300e38503bd93548ea6144bac6ed6b3b1
Ruairi Moran <ruairi.moran@equipmentshare.com>
2024-01-08 15:57:40 +0000

    comparing c++ function speeds

--- playground/test0.cpp
+++ playground/test0.cpp
@@ -3,30 +3,33 @@
 using namespace std;
 #define N 1e9
 
-void inline inline_add(int *p) { *p += 1; }
+void add_by_pointer(int* x) { 
+    *x += 1;
+};
 
-void outline_add(int *p) {
-    *p += 1;
-}
+void add_by_reference(int& loc_of_x) {
+    loc_of_x += 1;
+};
 
 
 int main() {
     int x = 1;
-    int *px = &x;
+    int* px = &x;
     clock_t time0 = clock();
     for (int i=0; i<N; i++) {
-        inline_add(px);
+        add_by_pointer(px);
     }
     clock_t time1 = clock();
     x = 1;
     clock_t time2 = clock();
     for (int i=0; i<N; i++) {
-        outline_add(px);
+        add_by_reference(x);
     }
     clock_t time3 = clock();
-    float in = (float) (time1 - time0) / CLOCKS_PER_SEC;
-    float out = (float) (time3 - time2) / CLOCKS_PER_SEC;
-    cout << "N = " << N << "\ninline: " << in << "\noutline: " << out << "\n";
-    // cout << "\n" << time0 << "\n" << time1 << "\n" << time2 << "\n" << time3;
+    float ptr = (float) (time1 - time0) / CLOCKS_PER_SEC;
+    float ref = (float) (time3 - time2) / CLOCKS_PER_SEC;
+    cout << "N = " << N << 
+    "\nby_ptr: " << ptr << 
+    "\nby_ref: " << ref << endl;
     return 0;
 }

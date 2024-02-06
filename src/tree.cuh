8233ced7db498b92836d6c738ae614d9e6df894d
ruairimoran <ruairi@moteam.com>
2024-02-06 19:41:12 +0000

    added default problem_data.json file to work from

--- src/tree.cuh
+++ src/tree.cuh
@@ -5,17 +5,6 @@
 #include <stdexcept>
 
 
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-
-
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true) {
-   if (code != cudaSuccess) {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
-}
-
-
 /**
  * Computing conditional probability of each tree node
  * @param[in] anc device ptr to ancestor of node at index

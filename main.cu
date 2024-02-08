d0b676cbd64fd78fc071dd29f732a79aa44b45a4
ruairimoran <ruairi@moteam.com>
2024-02-08 14:19:30 +0000

    rearranging cmakelists.txt files

--- main.cu
+++ main.cu
@@ -5,17 +5,24 @@
 */
 
 #include "src/tree.cuh"
+// #include "src/problem.cuh"
 #include "src/cones.cuh"
 #include <iostream>
 
 
 int main() {
-    std::ifstream file("tests/default_tree_data.json"); 
-    ScenarioTree tree(file);
-  	tree.print();
+    /** SCENARIO TREE */
+    std::ifstream fileTree("tests/default_tree_data.json"); 
+    ScenarioTree tree(fileTree);
+  	// tree.print();
 
+    /** PROBLEM DATA */
+    // std::ifstream fileProblem("tests/default_problem_data.json"); 
+    // ProblemData problem(tree, fileProblem);
+  	// problem.print();
+
+    /** CONES */
     // Context context; /* Create one context only */
-
     // /* Prepare some host and device data */
     // size_t n = 8;
     // DeviceVector<real_t> d_dataContainer(n);
@@ -23,17 +30,14 @@ int main() {
     // for (size_t i = 0; i < n; i=i+2) { dataHost[i] = -2. * (i + 1.); }
     // for (size_t i = 1; i < n; i=i+2) { dataHost[i] = 2. * (i + 1.); }
     // d_dataContainer.upload(dataHost);
-
     // /* Project to nonnegative orthant */
     // NonnegativeOrthantCone myCone(context);
     // myCone.projectOnCone(d_dataContainer.get(), d_dataContainer.capacity());
-
     // /* Get the data back to the host and print it */
     // std::vector<real_t> b;
     // d_dataContainer.download(b);
     // for (size_t i = 0; i < n; i++) std::cout << b[i] << " ";
     // std::cout << std::endl;
-
     // /* Project to SOC; incomplete! */
     // SecondOrderCone mySoc(context);
     // mySoc.projectOnCone(d_dataContainer.get(), d_dataContainer.capacity());
--- src/problem.cuh
+++ src/problem.cuh
@@ -1,6 +1,6 @@
 #include "../include/stdgpu.h"
-#include "tree.cuh"
-#include "cones.cuh"
+// #include "tree.cuh"
+// #include "cones.cuh"
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
@@ -41,13 +41,16 @@ class ProblemData {
         DeviceVector<real_t> m_d_inputDynamics;  ///< Ptr to
         DeviceVector<real_t> m_d_stateWeight;  ///< Ptr to
         DeviceVector<real_t> m_d_inputWeight;  ///< Ptr to
+        DeviceVector<real_t> m_d_stateWeightLeaf;  ///< Ptr to
         DeviceVector<real_t> m_d_stateConstraint;  ///< Ptr to
-        // DeviceVector<ConvexCone*> m_d_stateConstraintCone;  ///< Ptr to
+        DeviceVector<ConvexCone*> m_d_stateConstraintCone;  ///< Ptr to
         DeviceVector<real_t> m_d_inputConstraint;  ///< Ptr to
-        // DeviceVector<ConvexCone*> m_d_inputConstraintCone;  ///< Ptr to
+        DeviceVector<ConvexCone*> m_d_inputConstraintCone;  ///< Ptr to
+        DeviceVector<real_t> m_d_stateConstraintLeaf;  ///< Ptr to
+        DeviceVector<ConvexCone*> m_d_stateConstraintLeafCone;  ///< Ptr to
         DeviceVector<real_t> m_d_riskMatE;  ///< Ptr to
         DeviceVector<real_t> m_d_riskMatF;  ///< Ptr to
-        // DeviceVector<ConvexCone*> m_d_riskConeK;  ///< Ptr to
+        DeviceVector<ConvexCone*> m_d_riskConK;  ///< Ptr to
         DeviceVector<real_t> m_d_riskVecB;  ///< Ptr to
 
 	public:
@@ -70,74 +73,86 @@ class ProblemData {
             m_numInputs = doc["numInputs"].GetInt();
 
             /** Allocate memory on host for JSON data */
-            std::vector<real_t> hostSystemDynamics(m_numStates * m_numStates * m_tree.numEvents());
-            std::vector<real_t> hostInputDynamics(m_numStates * m_numInputs * m_tree.numEvents());
-            std::vector<real_t> hostStateWeight(m_numStates * m_numStates * m_tree.numEvents());
-            std::vector<real_t> hostInputWeight(m_numInputs * m_numInputs * m_tree.numEvents());
-            std::vector<real_t> hostStateWeightTerminal(m_numStates * m_numStates * m_tree.numEvents());
-            std::vector<real_t> hostStateConstraintMinMax(m_numStates * 2 * m_tree.numEvents());
-            std::vector<real_t> hostInputConstraintMinMax(m_numInputs * 2 * m_tree.numEvents());
-            std::vector<real_t> hostStateConstraintTerminalMinMax(m_numStates * 2 * m_tree.numEvents());
-            std::vector<real_t> hostRiskAlphas(m_tree.numEvents());
+            std::vector<real_t> jsonSystemDynamics(m_numStates * m_numStates * m_tree.numEvents());
+            std::vector<real_t> jsonInputDynamics(m_numStates * m_numInputs * m_tree.numEvents());
+            std::vector<real_t> jsonStateWeight(m_numStates * m_numStates * m_tree.numEvents());
+            std::vector<real_t> jsonInputWeight(m_numInputs * m_numInputs * m_tree.numEvents());
+            std::vector<real_t> jsonStateWeightLeaf(m_numStates * m_numStates * m_tree.numEvents());
+            std::vector<real_t> jsonStateConstraint(m_numStates * 2 * m_tree.numEvents());
+            std::vector<real_t> jsonInputConstraint(m_numInputs * 2 * m_tree.numEvents());
+            std::vector<real_t> jsonStateConstraintLeaf(m_numStates * 2 * m_tree.numEvents());
+            std::vector<real_t> jsonRiskAlphas(m_tree.numEvents());
 
             /** Allocate memory on device */
             m_d_systemDynamics.allocateOnDevice(m_numStates * m_numStates * m_tree.numNodes());
             m_d_inputDynamics.allocateOnDevice(m_numInputs * m_numInputs * m_tree.numNodes());
-            m_d_stateWeight.allocateOnDevice(m_numStates * m_numStates * (m_tree.numNodes() + m_tree.numLeafNodes()));
+            m_d_stateWeight.allocateOnDevice(m_numStates * m_numStates * m_tree.numNodes());
             m_d_inputWeight.allocateOnDevice(m_numInputs * m_numInputs * m_tree.numNodes());
+            m_d_stateWeightLeaf.allocateOnDevice(m_numStates * m_numStates * m_tree.numNodes());
             m_d_stateConstraint.allocateOnDevice(m_numStates * 2 * (m_tree.numNodes() + m_tree.numLeafNodes()));
-            // m_d_stateConstraintCone.allocateOnDevice(m_tree.numNodes());
+            m_d_stateConstraintCone.allocateOnDevice(m_tree.numNodes());
             m_d_inputConstraint.allocateOnDevice(m_numInputs * 2 * m_tree.numNodes());
-            // m_d_inputConstraintCone.allocateOnDevice(m_tree.numNodes());
-            m_d_riskMatE.allocateOnDevice((m_tree.numEvents() * 2 + 1) * (m_tree.numEvents()) * m_tree.numNodes());
-            m_d_riskMatF.allocateOnDevice((m_tree.numEvents() * 2 + 1) * (1) * m_tree.numNodes());
-            // m_d_riskConeK.allocateOnDevice(m_tree.numNodes());
-            m_d_riskVecB.allocateOnDevice(m_tree.numEvents() * 2 + 1);
+            m_d_stateConstraintLeafCone.allocateOnDevice(m_tree.numNodes());
+            m_d_riskMatE.allocateOnDevice((m_tree.numEvents() * 2 + 1) * (m_tree.numEvents()) * m_tree.numNonleafNodes());
+            m_d_riskMatF.allocateOnDevice((m_tree.numEvents() * 2 + 1) * (1) * m_tree.numNonleafNodes());
+            m_d_riskConK.allocateOnDevice(m_tree.numNonleafNodes());
+            m_d_riskVecB.allocateOnDevice((m_tree.numEvents() * 2 + 1) + m_tree.numNonleafNodes());
 
             /** Store array data from JSON in host memory */
             size_t len = m_numStates*m_numStates;
             for (rapidjson::SizeType i = 0; i<len; i++) {
-                hostSystemDynamics[i] = doc["systemDynamicsMode0"][i].GetDouble();
-                hostSystemDynamics[i+len] = doc["systemDynamicsMode1"][i].GetDouble();
-                hostStateWeight[i] = doc["stateWeightMode0"][i].GetDouble();
-                hostStateWeight[i+len] = doc["stateWeightMode1"][i].GetDouble();
-                hostStateWeightTerminal[i] = doc["stateWeightMode0"][i].GetDouble();
-                hostStateWeightTerminal[i+len] = doc["stateWeightMode1"][i].GetDouble();
+                jsonSystemDynamics[i] = doc["systemDynamicsMode0"][i].GetDouble();
+                jsonSystemDynamics[i+len] = doc["systemDynamicsMode1"][i].GetDouble();
+                jsonStateWeight[i] = doc["stateWeightMode0"][i].GetDouble();
+                jsonStateWeight[i+len] = doc["stateWeightMode1"][i].GetDouble();
+                jsonStateWeightLeaf[i] = doc["stateWeightLeafMode0"][i].GetDouble();
+                jsonStateWeightLeaf[i+len] = doc["stateWeightLeafMode1"][i].GetDouble();
             }
             len = m_numInputs*m_numInputs;
             for (rapidjson::SizeType i = 0; i<len; i++) {
-                hostInputDynamics[i] = doc["controlDynamicsMode0"][i].GetDouble();
-                hostInputDynamics[i+len] = doc["controlDynamicsMode1"][i].GetDouble();
-                hostInputWeight[i] = doc["inputWeightMode0"][i].GetDouble();
-                hostInputWeight[i+len] = doc["inputWeightMode1"][i].GetDouble();
+                jsonInputDynamics[i] = doc["controlDynamicsMode0"][i].GetDouble();
+                jsonInputDynamics[i+len] = doc["controlDynamicsMode1"][i].GetDouble();
+                jsonInputWeight[i] = doc["inputWeightMode0"][i].GetDouble();
+                jsonInputWeight[i+len] = doc["inputWeightMode1"][i].GetDouble();
             }
             len = m_numStates * 2;
             for (rapidjson::SizeType i = 0; i<len; i++) {
-                hostStateConstraintMinMax[i] = doc["stateConstraintMode0"][i].GetDouble();
-                hostStateConstraintMinMax[i+len] = doc["stateConstraintMode1"][i].GetDouble();
-                hostStateConstraintTerminalMinMax[i] = doc["stateConstraintTerminalMode0"][i].GetDouble();
-                hostStateConstraintTerminalMinMax[i+len] = doc["stateConstraintTerminalMode1"][i].GetDouble();
+                jsonStateConstraint[i] = doc["stateConstraintMode0"][i].GetDouble();
+                jsonStateConstraint[i+len] = doc["stateConstraintMode1"][i].GetDouble();
+                jsonStateConstraintLeaf[i] = doc["stateConstraintLeafMode0"][i].GetDouble();
+                jsonStateConstraintLeaf[i+len] = doc["stateConstraintLeafMode1"][i].GetDouble();
             }
             len = m_numInputs * 2;
             for (rapidjson::SizeType i = 0; i<len; i++) {
-                hostInputConstraintMinMax[i] = doc["inputConstraintMode0"][i].GetDouble();
-                hostInputConstraintMinMax[i+len] = doc["inputConstraintMode1"][i].GetDouble();
+                jsonInputConstraint[i] = doc["inputConstraintMode0"][i].GetDouble();
+                jsonInputConstraint[i+len] = doc["inputConstraintMode1"][i].GetDouble();
             }
-            hostRiskAlphas[0] = doc["riskMode0"][0].GetDouble();
-            hostRiskAlphas[0] = doc["riskMode1"][0].GetDouble();
+            jsonRiskAlphas[0] = doc["riskMode0"][0].GetDouble();
+            jsonRiskAlphas[0] = doc["riskMode1"][0].GetDouble();
 
-            /** Transfer JSON array data to device */
-            m_d_systemDynamics.upload(repeat_n(hostSystemDynamics, m_tree.numNodes()));
+            /** Create full arrays on host */
+            // std::vector<real_t> hostSystemDynamics(m_d_systemDynamics.capacity());
+            // for (size_t i=0; i<m_tree.numNodes(); i++) {
+            //     size_t event = m_tree.events()[i];
+            //     hostSystemDynamics.insert(hostSystemDynamics.end(), 
+            //         jsonSystemDynamics[event * len], 
+            //         jsonSystemDynamics[event * len + len]);
+            // }
+
+            /** Transfer array data to device */
+            // m_d_systemDynamics.upload(hostSystemDynamics);
             // m_d_inputDynamics.upload();
             // m_d_stateWeight.upload();
             // m_d_inputWeight.upload();
+            // m_d_stateWeightLeaf.upload();
             // m_d_stateConstraint.upload();
             // m_d_stateConstraintCone.upload();
             // m_d_inputConstraint.upload();
             // m_d_inputConstraintCone.upload();
+            // m_d_stateConstraintLeaf.upload();
             // m_d_riskMatE.upload();
             // m_d_riskMatF.upload();
-            // m_d_riskConeK.upload();
+            // m_d_riskConK.upload();
             // m_d_riskVecB.upload();
         }
 
@@ -156,27 +171,31 @@ class ProblemData {
         DeviceVector<real_t>& stateWeight() { return m_d_stateWeight; }
         DeviceVector<real_t>& inputWeight() { return m_d_inputWeight; }
         DeviceVector<real_t>& stateConstraint() { return m_d_stateConstraint; }
-        // DeviceVector<ConvexCone*>& stateConstraintCone() { return m_d_stateConstraintCone; }
+        DeviceVector<ConvexCone*>& stateConstraintCone() { return m_d_stateConstraintCone; }
         DeviceVector<real_t>& inputConstraint() { return m_d_inputConstraint; }
-        // DeviceVector<ConvexCone*>& inputConstraintCone() { return m_d_inputConstraintCone; }
+        DeviceVector<ConvexCone*>& inputConstraintCone() { return m_d_inputConstraintCone; }
+        DeviceVector<real_t>& stateConstraintLeaf() { return m_d_stateConstraintLeaf; }
+        DeviceVector<ConvexCone*>& stateConstraintLeafCone() { return m_d_stateConstraintLeafCone; }
         DeviceVector<real_t>& riskMatE() { return m_d_riskMatE; }
         DeviceVector<real_t>& riskMatF() { return m_d_riskMatF; }
-        // DeviceVector<ConvexCone*>& riskConeK() { return m_d_riskConeK; }
+        DeviceVector<ConvexCone*>& riskConK() { return m_d_riskConK; }
         DeviceVector<real_t>& riskVecB() { return m_d_riskVecB; }
 
         /**
          * Debugging
          */
-		// void print(){
-        //     std::vector<int> hostDataIntNumNodes(m_numNodes);
+		void print(){
+            size_t len = m_numStates * m_numStates * m_tree.numNodes();
+            std::vector<real_t> hostData(len);
 
-		// 	std::cout << "Number of nonleaf nodes: " << m_numNonleafNodes << std::endl;
+			std::cout << "Number of states: " << m_numStates << std::endl;
+            std::cout << "Number of inputs: " << m_numInputs << std::endl;
 
-        //     m_d_stages.download(hostDataIntNumNodes);
-        //     std::cout << "Stages (from device): ";
-        //     for (size_t i=0; i<m_numNodes; i++) {
-        //         std::cout << hostDataIntNumNodes[i] << " ";
-        //     }
-        //     std::cout << std::endl;
-		// }
+            m_d_systemDynamics.download(hostData);
+            std::cout << "System dynamics (from device): ";
+            for (size_t i=0; i<len; i++) {
+                std::cout << hostData[i] << " ";
+            }
+            std::cout << std::endl;
+		}
 };

4ca8a32584409ee23bd14478df7e0be4ef87f59a
Ruairi Moran <ruairi.moran@equipmentshare.com>
2024-05-27 19:50:57 +0800

    multiple runs and average tests

--- playground/play.cu
+++ playground/play.cu
@@ -10,11 +10,23 @@
 
 
 template<typename T>
+T avgTime(std::vector<T> &times) {
+    size_t num = times.size();
+    T sum = 0;
+    for (size_t i = 0; i < num; i++) {
+        sum += times[i];
+    }
+    T avg = sum / num;
+    return avg;
+}
+
+
+template<typename T>
 void projectionSerial(size_t dim, std::vector<T> &vec) {
     std::vector<T> vecFirstPart(vec.begin(), vec.end() - 1);
     std::vector<T> squares(dim - 1);
     T sum = 0;
-    for (size_t i=0;i<dim-2;i++) {
+    for (size_t i = 0; i < dim - 2; i++) {
         T temp = vecFirstPart[i];
         squares[i] = temp * temp;
         sum += squares[i];
@@ -35,29 +47,42 @@ void projectionSerial(size_t dim, std::vector<T> &vec) {
 
 int main() {
     /* Create data */
+    size_t runs = 10;
     size_t coneDim = 100;
     size_t numCones = 1000;
     std::vector<real_t> numbers(coneDim * numCones);
     std::iota(std::begin(numbers), std::end(numbers), 0);
 
     /* Test parallel projection */
-    DTensor<real_t> d_dataB(numbers, coneDim, numCones);
-    SocProjection socPara(d_dataB);
-    const auto tickB = std::chrono::high_resolution_clock::now();
-    socPara.project(d_dataB);
-    const auto tockB = std::chrono::high_resolution_clock::now();
-    double durationMilliB = std::chrono::duration<double, std::milli>(tockB - tickB).count();
-    std::cout << "parallel timer stopped:  " << durationMilliB << " ms" << "\n";
+    std::vector<real_t> timesB(runs);
+    std::vector<DTensor<real_t>> dataB(runs);
+    for (size_t i = 0; i < runs; i++) {
+        dataB[i] = DTensor<real_t>(numbers, coneDim, numCones);
+        SocProjection socPara(dataB[i]);
+        const auto tickB = std::chrono::high_resolution_clock::now();
+        socPara.project(dataB[i]);
+        const auto tockB = std::chrono::high_resolution_clock::now();
+        timesB[i] = std::chrono::duration<real_t, std::milli>(tockB - tickB).count();
+    }
+    real_t avgB = avgTime(timesB);
+    std::cout << "parallel: " << avgB << " ms" << "\n";
 
     /* Test serial projection */
-    std::vector<std::vector<real_t>> split(numCones);
-    for (size_t i = 0; i < numCones; i++) { split[i] = std::vector<real_t>(numbers.begin() + coneDim * i,
-                                                                           numbers.begin() + coneDim * (i + 1)); }
-    const auto tickC = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < numCones; i++) { projectionSerial(coneDim, split[i]); }
-    const auto tockC = std::chrono::high_resolution_clock::now();
-    double durationMilliC = std::chrono::duration<double, std::milli>(tockC - tickC).count();
-    std::cout << "serial timer stopped:  " << durationMilliC << " ms" << "\n";
+    std::vector<real_t> timesC(runs);
+    std::vector<std::vector<std::vector<real_t>>> dataC(runs);
+    for (size_t i = 0; i < runs; i++) {
+        dataC[i].resize(numCones);
+        for (size_t j = 0; j < numCones; j++) {
+            dataC[i][j] = std::vector<real_t>(numbers.begin() + coneDim * j,
+                                              numbers.begin() + coneDim * (j + 1));
+        }
+        const auto tickC = std::chrono::high_resolution_clock::now();
+        for (size_t j = 0; j < numCones; j++) { projectionSerial(coneDim, dataC[i][j]); }
+        const auto tockC = std::chrono::high_resolution_clock::now();
+        timesC[i] = std::chrono::duration<double, std::milli>(tockC - tickC).count();
+    }
+    real_t avgC = avgTime(timesC);
+    std::cout << "serial: " << avgC << " ms" << "\n";
 
     cudaDeviceReset();
     return 0;

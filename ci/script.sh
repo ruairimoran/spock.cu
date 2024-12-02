c87526f4239e866d8841b79052b696a0b83edbb8
Ruairi Moran <ruairi.moran@equipmentshare.com>
2024-12-02 12:16:02 +0000

    change cmakelists for testing

--- ci/script.sh
+++ ci/script.sh
@@ -36,7 +36,7 @@ test_python() {
 # ------------------------------------
 test_cpp() {
     # -- create build files
-    cmake -S . -B ./build -Wno-dev -DSPOCK_BUILD_TESTING=ON
+    cmake -S . -B ./build -Wno-dev -SPOCK_BUILD_TESTING=ON
 
     # -- build files in build folder
     cmake --build ./build

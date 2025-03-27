b059ffad20e10151cb2d0a710e5176df3f0e1b8f
Ruairi Moran <ruairi.moran@equipmentshare.com>
2025-03-27 18:15:58 +0000

    update python problem and shell script for testing different branching

--- tests/julia/julia.jl
+++ tests/julia/julia.jl
@@ -101,7 +101,7 @@ data = read_data()
 risk = build_risk(data)
 x0 = read_vector_from_binary(TR, folder * "initialState" * file_ext_r)
 tol = 1e-3
-max_time = .25 * minute
+max_time = 5 * minute
 status = Int64(1)
 tol_f64 = Float64(tol)
 max_time_f64 = Float64(max_time)
@@ -124,7 +124,8 @@ for run in [0, 1]
         println("Saving julia times...")
         num_vars = data.num_nodes * (data.num_states + data.num_inputs)
         open("time.csv", "a") do f
-            write(f, "$(num_vars), $(time_g), $(ram_g), $(time_m), $(ram_m), $(time_i), $(ram_i), ")
+            write(f, "$(data.ch_num[1]), $(time_g), $(time_m), $(time_i), ")
+            # write(f, "$(num_vars), $(time_g), $(ram_g), $(time_m), $(ram_m), $(time_i), $(ram_i), ")
             # write(f, "$(num_vars), $(time_m), $(ram_m), ")
         end
         println("Saved!")

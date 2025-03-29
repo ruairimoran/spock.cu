c5686ac41b48708b8f07643dbb952ea35fa5fa3d
Ruairi Moran <ruairi.moran@equipmentshare.com>
2025-03-29 16:42:50 +0000

    test poly

--- examples/precondition/run.sh
+++ examples/precondition/run.sh
@@ -8,11 +8,7 @@ main() {
     # shellcheck disable=SC1090
     source "${path}.venv/bin/activate"
     mkdir -p ./build
-    python main.py --dt="d" --precondition=1
-    cmake -S $path -B ./build -Wno-dev
-    cmake --build ./build
-    ./build/examples/precondition/precondition
-    python main.py --dt="d" --precondition=0
+    python main.py --dt="d"
     cmake -S $path -B ./build -Wno-dev
     cmake --build ./build
     ./build/examples/precondition/precondition

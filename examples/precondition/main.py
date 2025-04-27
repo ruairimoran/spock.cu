a0e301c34184170fa60857d6bffaf45ea6b636bd
Ruairi Moran <ruairi.moran@equipmentshare.com>
2025-04-27 17:57:25 +0100

    quadratic-plus-linear costs giving same result as mosek

--- examples/precondition/main.py
+++ examples/precondition/main.py
@@ -4,21 +4,13 @@ import spock as s
 import cvxpy as cp
 
 
-def check_spd(mat, name):
-    eigs = np.linalg.eigvals(mat)
-    is_positive_definite = eigs.all() > 0
-    is_symmetric = np.allclose(mat, mat.T)
-    print("Is " + name + " symmetric positive-definite?", is_positive_definite and is_symmetric,
-          " with norm (", np.linalg.norm(mat), ").")
-
-
 parser = argparse.ArgumentParser(description='Example: preconditioning.')
 parser.add_argument("--dt", type=str, default='d')
 args = parser.parse_args()
 dt = args.dt
 
 # Sizes
-horizon = 3
+horizon = 2
 stopping = 1
 num_events = 2
 num_inputs = 1
@@ -44,46 +36,40 @@ print(tree)
 # --------------------------------------------------------
 # Dynamics
 dynamics = []
-A_base = np.eye(num_states) * 1.
+A_base = np.eye(num_states)
 B_base = np.ones((num_states, num_inputs)) * .5
 for i in range(1, num_events + 1):
-    A = A_base * i
-    B = B_base * i
+    A = A_base * 1/i
+    B = B_base * 1/i
     dynamics += [s.build.Dynamics(A, B)]
 
 # Costs
 rng = np.random.default_rng()
 nonleaf_costs = []
-Q_base = np.eye(num_states) * 1.
-R_base = np.eye(num_inputs) * 3.
+zero = 1e-1
+Q_base = np.eye(num_states) * zero
+R_base = np.eye(num_inputs) * zero
 for i in range(1, num_events + 1):
-    # Q_noise = np.diag(rng.normal(0., .1, size=num_states))
-    # R_noise = np.diag(rng.normal(0., .1, size=num_inputs))
-    Q = Q_base# + Q_noise
-    R = R_base * i# + R_noise
-    check_spd(Q, "Q")
-    check_spd(R, "R")
-    q = np.ones(num_states)
-    r = np.ones(num_inputs)
+    Q = Q_base
+    R = R_base
+    q = np.ones(num_states) * zero
+    r = np.ones(num_inputs) * i * 3
     nonleaf_costs += [s.build.CostQuadraticPlusLinear(Q, q, R, r)]
-    # nonleaf_costs += [s.build.CostQuadratic(Q, R)]
 
 T = Q_base
-check_spd(T, "T")
-t = np.zeros(num_states)
+t = np.ones(num_states) * zero
 leaf_cost = s.build.CostQuadraticPlusLinear(T, t, leaf=True)
-# leaf_cost = s.build.CostQuadratic(T, leaf=True)
 
 # Constraints
-nonleaf_state_ub = np.ones(num_states) * 10.
-nonleaf_state_lb = -nonleaf_state_ub
-nonleaf_input_ub = np.ones(num_inputs) * 6.
+nonleaf_state_ub = np.ones(num_states) * 5.
+nonleaf_state_lb = np.zeros(num_states)
+nonleaf_input_ub = np.ones(num_inputs) * 4.
 nonleaf_input_lb = -nonleaf_input_ub
 nonleaf_lb = np.hstack((nonleaf_state_lb, nonleaf_input_lb))
 nonleaf_ub = np.hstack((nonleaf_state_ub, nonleaf_input_ub))
 nonleaf_constraint = s.build.Polyhedron(np.eye(num_states + num_inputs), nonleaf_lb, nonleaf_ub)
 leaf_ub = np.ones(num_states) * 1.
-leaf_lb = -leaf_ub
+leaf_lb = np.zeros(num_states)
 leaf_constraint = s.build.Rectangle(leaf_lb, leaf_ub)
 
 # Risk
@@ -108,21 +94,21 @@ problem = (
 )
 print("Unconditioned: \n", problem)
 
-# problem_cond = (
-#     s.problem.Factory(
-#         scenario_tree=tree,
-#         num_states=num_states,
-#         num_inputs=num_inputs)
-#     .with_dynamics_events(dynamics)
-#     .with_cost_nonleaf_events(nonleaf_costs)
-#     .with_cost_leaf(leaf_cost)
-#     .with_constraint_nonleaf(nonleaf_constraint)
-#     .with_constraint_leaf(leaf_constraint)
-#     .with_risk(risk)
-#     .with_preconditioning(True)
-#     .generate_problem()
-# )
-# print("Preconditioned: \n", problem_cond)
+problem_cond = (
+    s.problem.Factory(
+        scenario_tree=tree,
+        num_states=num_states,
+        num_inputs=num_inputs)
+    .with_dynamics_events(dynamics)
+    .with_cost_nonleaf_events(nonleaf_costs)
+    .with_cost_leaf(leaf_cost)
+    .with_constraint_nonleaf(nonleaf_constraint)
+    .with_constraint_leaf(leaf_constraint)
+    .with_risk(risk)
+    .with_preconditioning(True)
+    .generate_problem()
+)
+print("Preconditioned: \n", problem_cond)
 
 # Initial state
 x0 = np.ones(num_states) * 5.
@@ -139,29 +125,29 @@ def run_unconditioned(sol):
     return model.states, model.inputs, model.primal
 
 
-# def run_conditioned(sol):
-#     model = s.model.ModelWithPrecondition(tree, problem_cond)
-#     model.solve(x0, sol, tol=1e-8)
-#     print("---- Preconditioned problem ----")
-#     print(sol.__str__(), "preconditioned status: ", model.status)
-#     return model.states, model.inputs, model.status
+def run_conditioned(sol):
+    model = s.model.ModelWithPrecondition(tree, problem_cond)
+    model.solve(x0, sol, tol=1e-8)
+    print("---- Preconditioned problem ----")
+    print(sol.__str__(), "preconditioned status: ", model.status)
+    return model.states, model.inputs, model.status
 
 
+np.set_printoptions(suppress=True)
 try:
     solver = cp.MOSEK
     states, inputs, primal = run_unconditioned(solver)
 except:
     solver = cp.SCS
     states, inputs, primal = run_unconditioned(solver)
-# states_, inputs_, status = run_conditioned(solver)
-# if status != "infeasible":
-#     tol = 1e-2
-#     print("States:\n", states_, "\nInputs:\n", inputs_, "\n")
-#     print("Equal: ",
-#           np.allclose(states_, states, tol) and
-#           np.allclose(inputs_, inputs, tol),
-#           "\n")
+states_, inputs_, status = run_conditioned(solver)
+if status != "infeasible":
+    tol = 1e-2
+    print("States:\n", states_, "\nInputs:\n", inputs_, "\n")
+    print("Equal: ",
+          np.allclose(states_, states, tol) and
+          np.allclose(inputs_, inputs, tol),
+          "\n")
 
 # Primal solution
 tree.write_to_file_fp("primal", primal)
-print(primal.size)

7082ec0b12d88e29940f6e680f176721a04e5a9d
Ruairi Moran <ruairi.moran@equipmentshare.com>
2025-05-02 15:18:57 +0100

    fix bash script

--- examples/networkedControl/time.sh
+++ examples/networkedControl/time.sh
@@ -13,16 +13,16 @@ main() {
     branching=0
     for ch in $(seq 2 $n); do
         for branching in {0..1}; do
-    python main.py --dt="d" --br="$branching" --ch="$ch" --tree=1
-    julia ../../tests/julia/julia.jl |& tee log/julia.txt
-    exit_code=$?
-    if [ $exit_code -eq 0 ]; then
-        cmake -S $path -B ./build -Wno-dev
-        cmake --build ./build
-        ./build/examples/networkedControl/networked |& tee log/spock.txt
-    else
-        printf "Julia error!"
-    fi
+            python main.py --dt="d" --br="$branching" --ch="$ch" --tree=1
+            julia ../../tests/julia/julia.jl |& tee log/julia.txt
+            exit_code=$?
+            if [ $exit_code -eq 0 ]; then
+                cmake -S $path -B ./build -Wno-dev
+                cmake --build ./build
+                ./build/examples/networkedControl/networked |& tee log/spock.txt
+            else
+                printf "Julia error!"
+            fi
         done
     done
 }
